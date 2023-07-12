198be6e960f35c7ae351844e5878bf4a4741b29a
Innei <i@innei.in>
2023-07-12 15:19:33 +0800

    update

---
+++ lua/plugins/buffer.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "akinsho/bufferline.nvim",
+    event = "VeryLazy",
+    keys = {
+      { "<leader>bp", "<Cmd>BufferLineTogglePin<CR>", desc = "Toggle pin" },
+      { "<leader>bP", "<Cmd>BufferLineGroupClose ungrouped<CR>", desc = "Delete non-pinned buffers" },
+      { "<leader>bq", "<Cmd>BufferLineCloseOthers<CR>", desc = "Delete other buffers" },
+    },
+  },
+}
