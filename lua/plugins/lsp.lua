006b8e5744e0f9421e8b97f014c72c81de885c9e
Innei <i@innei.in>
2023-08-10 22:25:40 +0800

    lsp rename file

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -324,26 +324,14 @@ return {
       set_keymap("gpr", "<cmd>lua require('goto-preview').goto_preview_references()<CR>", "Go to references preview")
     end,
   },
-  -- {
-  --   -- Not needed anymore with nvim-0.10.0
-  --   {
-  --     "lvimuser/lsp-inlayhints.nvim",
-  --     branch = "anticonceal",
-  --     event = "LspAttach",
-  --     opts = {},
-  --     config = function(_, opts)
-  --       require("lsp-inlayhints").setup(opts)
-  --       vim.api.nvim_create_autocmd("LspAttach", {
-  --         group = vim.api.nvim_create_augroup("LspAttach_inlayhints", {}),
-  --         callback = function(args)
-  --           if not (args.data and args.data.client_id) then
-  --             return
-  --           end
-  --           local client = vim.lsp.get_client_by_id(args.data.client_id)
-  --           require("lsp-inlayhints").on_attach(client, args.buf)
-  --         end,
-  --       })
-  --     end,
-  --   },
-  -- },
+  {
+    "antosha417/nvim-lsp-file-operations",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-neo-tree/neo-tree.nvim",
+    },
+    config = function()
+      require("lsp-file-operations").setup()
+    end,
+  },
 }
