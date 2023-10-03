e3fdc104132ad37106befeec3778761b621749b3
Innei <i@innei.in>
2023-10-03 13:37:24 +0800

    update

--- lua/plugins/case.lua
+++ lua/plugins/case.lua
@@ -7,14 +7,13 @@ return {
     require("textcase").setup({})
     require("telescope").load_extension("textcase")
 
-    vim.api.nvim_set_keymap("n", "ga.", "<cmd>TextCaseOpenTelescope<CR>", { desc = "Telescope" })
-    vim.api.nvim_set_keymap("v", "ga.", "<cmd>TextCaseOpenTelescope<CR>", { desc = "Telescope" })
+    vim.api.nvim_set_keymap("n", "ga.", "<cmd>TextCaseOpenTelescopeLSPChange<CR>", { desc = "Telescope" })
+    vim.api.nvim_set_keymap("v", "ga.", "<cmd>TextCaseOpenTelescopeLSPChange<CR>", { desc = "Telescope" })
     vim.api.nvim_set_keymap(
       "n",
       "gaa",
       "<cmd>TextCaseOpenTelescopeQuickChange<CR>",
       { desc = "Telescope Quick Change" }
     )
-    vim.api.nvim_set_keymap("n", "gai", "<cmd>TextCaseOpenTelescopeLSPChange<CR>", { desc = "Telescope LSP Change" })
   end,
 }
