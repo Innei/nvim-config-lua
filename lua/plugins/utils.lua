03ab9d50bb4ceb84a023e137bdc7a33f23fe7d58
Innei <i@innei.in>
2023-10-12 11:43:16 +0800

    updatre

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -44,4 +44,18 @@ return {
       })
     end,
   },
+
+  {
+    "sontungexpt/url-open",
+    event = "VeryLazy",
+    cmd = "URLOpenUnderCursor",
+    config = function()
+      local status_ok, url_open = pcall(require, "url-open")
+      if not status_ok then
+        return
+      end
+      url_open.setup({})
+      vim.keymap.set("n", "gx", "<esc>:URLOpenUnderCursor<cr>")
+    end,
+  },
 }
