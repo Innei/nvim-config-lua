f8ec97dce83c4197d38ef7eea77195711f541b10
Innei <i@innei.in>
2023-12-23 20:29:39 +0800

    fix: align

--- lua/plugins/align.lua
+++ lua/plugins/align.lua
@@ -1,20 +1,54 @@
 return {
   "Vonr/align.nvim",
   event = "BufRead",
+  branch = "v2",
   config = function()
     local NS = { noremap = true, silent = true }
 
+    -- Aligns to 1 character
     vim.keymap.set("x", "aa", function()
-      require("align").align_to_char(1, true)
-    end, NS) -- Aligns to 1 character, looking le   ft
-    vim.keymap.set("x", "as", function()
-      require("align").align_to_char(2, true, true)
-    end, NS) -- Aligns to 2 characters, looking le  ft and with previews
+      require("align").align_to_char({
+        length = 1,
+      })
+    end, NS)
+
+    -- Aligns to 2 characters with previews
+    vim.keymap.set("x", "ad", function()
+      require("align").align_to_char({
+        preview = true,
+        length = 2,
+      })
+    end, NS)
+
+    -- Aligns to a string with previews
     vim.keymap.set("x", "aw", function()
-      require("align").align_to_string(false, true, true)
-    end, NS) -- Aligns to a string, looking le      ft and with previews
+      require("align").align_to_string({
+        preview = true,
+        regex = false,
+      })
+    end, NS)
+
+    -- Aligns to a Vim regex with previews
     vim.keymap.set("x", "ar", function()
-      require("align").align_to_string(true, true, true)
-    end, NS) -- Aligns to a Lua pattern, looking le ft and with previews
+      require("align").align_to_string({
+        preview = true,
+        regex = true,
+      })
+    end, NS)
+
+    -- Example gawip to align a paragraph to a string with previews
+    vim.keymap.set("n", "gaw", function()
+      local a = require("align")
+      a.operator(a.align_to_string, {
+        regex = false,
+        preview = true,
+      })
+    end, NS)
+
+    -- Example gaaip to align a paragraph to 1 character
+    vim.keymap.set("n", "gaa", function()
+      local a = require("align")
+      a.operator(a.align_to_char)
+    end, NS)
   end,
 }
